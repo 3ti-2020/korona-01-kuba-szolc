1e1df815bd061fc35767c9f45fb91e53abc2fae0
Kubaszolc01 <kubaszolc01@gmail.com>
2020-05-13 01:05:21 +0200

    menu hamburgerowe

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("test")
+const menuBurger = document.querySelector(".menuBurger");
+const menuContent = document.querySelector(".menuContent");
+
+menuBurger.addEventListener("click", function(){
+    menuBurger.classList.toggle("menuBurger--active");
+    menuContent.classList.toggle("menuContent--active");
+})
