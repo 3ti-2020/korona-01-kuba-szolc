e0ef7a68c7f9e2bb4c783b6a8b1fdbca78a0f9fe
Kubaszolc01 <42960682+Kubaszolc01@users.noreply.github.com>
2020-05-19 17:39:05 +0200

    90% css

--- index.js
+++ index.js
@@ -4,4 +4,25 @@ const menuContent = document.querySelector(".menuContent");
 menuBurger.addEventListener("click", function(){
     menuBurger.classList.toggle("menuBurger--active");
     menuContent.classList.toggle("menuContent--active");
-})
+});
+
+
+const filtersButton = document.querySelector(".filtersButton");
+const filtersBox = document.querySelector(".filtersBox");
+
+filtersButton.addEventListener("click", function(){
+    filtersBox.classList.toggle("filtersBox--active");
+});
+
+// const headerContent = document.querySelector(".headerContent");
+// for(i=0; i<5; i++){
+//     setTimeout(function(){
+//         headerContent.style.backgroundImage = "url('img/mac.jpg')";
+//     }, 2000);
+//     setTimeout(function(){
+//         headerContent.style.backgroundImage = "url('img/samsung.jpg')";
+//     }, 4000);
+//     setTimeout(function(){
+//         headerContent.style.backgroundImage = "url('img/xiaomi.jpg')";
+//     }, 6000);
+// }
